f6a687c814323e8813f3e892dfcc64cf79cd2fa7
Gleb Bogomolov <glebbogomolov04@gmail.com>
2024-04-18 12:57:36 +0300

    Adding interface IService

---
+++ TelecomApp.Shared/Interfaces/IService.cs
@@ -0,0 +1,18 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace TelecomApp.Shared.Interfaces
+{
+    public interface IService<TEntity, TEdit, TIDType>
+    {
+        public List<TEntity> GetAll();
+        public List<TEntity> GetAllFull();
+        public Task<TEntity?> GetByID(TIDType id);
+        public Task<TEntity?> GetByIDFull(TIDType id);
+
+        public Task<TEntity?> Update(TIDType id, TEdit editModel);
+
+        public Task<TEntity?> Create(TEdit editModel);
+
+        public Task<bool> Delete(TIDType id);
+    }
+}

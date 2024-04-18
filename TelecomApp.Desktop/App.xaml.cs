d2fcb69c0f1d638ff119a74dae120b05c4b4cde3
Gleb Bogomolov <glebbogomolov04@gmail.com>
2024-04-18 14:15:07 +0300

    Adding DbInit on startup

--- TelecomApp.Desktop/App.xaml.cs
+++ TelecomApp.Desktop/App.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using TelecomApp.Desktop.DataAccess;
 
 namespace TelecomApp.Desktop
 {
@@ -13,5 +15,10 @@ namespace TelecomApp.Desktop
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Database.SetInitializer(new DataInitializer());
+        }
     }
 }

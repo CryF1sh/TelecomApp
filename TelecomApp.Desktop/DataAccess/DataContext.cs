d349a86335f2dddbe1522f177de3a6e8953a292d
Gleb Bogomolov <glebbogomolov04@gmail.com>
2024-04-18 13:47:52 +0300

    Adding DbContext

---
+++ TelecomApp.Desktop/DataAccess/DataContext.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.Data.Entity;
+using TelecomApp.Desktop.Models;
+
+namespace TelecomApp.Desktop.DataAccess
+{
+    internal class DataContext : DbContext
+    {
+        public DataContext()
+        {
+        }
+
+        public DbSet<Abonent> Abonents { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<PhoneNumber> PhoneNumbers { get; set; }
+        public DbSet<Street> Streets { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
